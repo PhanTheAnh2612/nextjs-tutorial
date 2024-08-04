eec3a0b516f4d0e9c05a3c16bea8b00c11b29c84
Phan The Anh <phantheanh2612@gmail.com>
2024-08-05 01:01:19 +0700

    Update alsdc

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -58,9 +58,9 @@ export async function deleteUser(params: DeleteUserParams) {
 
     // Delte user from Database
     // and questions, answers, comments, etc.
-    const userQuestionIds = await Question.find({ author: user._id }).distinct(
-      "_id"
-    );
+    // const userQuestionIds = await Question.find({ author: user._id }).distinct(
+    //   "_id"
+    // );
 
     await Question.deleteMany({ author: user._id });
 
